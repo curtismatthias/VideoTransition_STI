af419da77ddb2ac026a437a99fa1e706a32ea488
Curtis Matthias <curtismatthias@hotmail.com>
2016-11-28 13:10:36 -0800

    Added STI using row, cleaned up code

--- Project(new)/VTSTI/main.cpp
+++ Project(new)/VTSTI/main.cpp
@@ -1,65 +1,104 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/opencv.hpp"
-#include <algorithm>
-#include <vector>
 
 using namespace std;
 using namespace cv;
 
+
 int main() {
 
-	Mat frame;
-	string filename;
-
-	filename = "test.mp4";
-
+	// video source
+	string filename = "test4.mp4";
 	VideoCapture cap(filename);
 
+	// video specifications
 	int totalFrames = cap.get(CV_CAP_PROP_FRAME_COUNT);
 	int frameWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
 	int frameHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+	int x_center = frameWidth/2;
+	int y_center = frameHeight/2;
 
+	// matrices
+	Mat frame;
 	Mat STI_init;
-	Mat STI;
+	Mat STI_col;
+	Mat STI_row;
 
+	// initialize STI matrices to frame template
+	cap >> STI_init;
+	cap >> STI_col;
+	cap >> STI_row;
+
+	// check if video file is valid
 	if(!cap.isOpened()){
-		cout << "Video cannot be opened" << endl;
+		std::cout << "\nVideo File Cannot be Opened" << std::endl;
+		std::cout << "\nPress Enter to Exit..." << std::endl;
+		std::cin.get();
 		return 1;
 	}
 
-	int x = frameWidth/2;
-	int y;
-	int f = 0; // frames of video
+	// resize STI matrices to correct resolution
+	resize(STI_init, STI_col, Size(totalFrames-3, frameHeight));
+	resize(STI_init, STI_row, Size(totalFrames-3, frameWidth));
 
-	cap >> STI_init;
-	cap >> STI;
-	resize(STI_init, STI, Size(totalFrames, frameHeight));
-
-	while(waitKey(30) != 27 && totalFrames>2){
-		cap >> frame;
-		imshow("Video", frame);
-		totalFrames--;
-
-		for(int i = 0; i < frameHeight;  i++){
-			y = i;
-		    Vec3b pixel = frame.at<Vec3b>(Point(x,y));
-			STI.at<Vec3b>(Point(f,i)) = pixel;
-			imshow("STI", STI);
+	// initialize STI images to black
+	Vec3b black; black[0] = 0; black[1] = 0; black[2] = 0; // define black pixel (0, 0, 0)
+	for(int i = 0; i < totalFrames-3;  i++){
+		for(int j = 0; j < frameHeight;  j++){
+			STI_col.at<Vec3b>(Point(i,j)) = black;
 		}
-
-		f++;
+		for(int j = 0; j < frameWidth;  j++){
+			STI_row.at<Vec3b>(Point(i,j)) = black;
+		}
 	}
 
-	//namedWindow("STI", CV_WINDOW_AUTOSIZE);
+	// determine if frame is larger in x or y
+	int STIsize;
+	if(frameWidth > frameHeight){
+		STIsize = frameWidth;
+	}
+	else{
+		STIsize = frameHeight;
+	}
 
-	/*for (std::vector<Vec3b>::iterator i = STIimage.begin(); i != STIimage.end(); i++){
-		cout << *i << endl;
-	}*/
+	int frameNum = 0; // current frame number
 
-	cout << "Press Enter to Exit..." << endl;
-	cin.get();
+	// loop through every frame of the video source
+	while(waitKey(30) != 27 && totalFrames>3){
+		cap >> frame;
+		imshow("Video Source", frame);
+		totalFrames--;
+
+		// loop through all pixels in column/row
+		for(int i = 0; i < STIsize;  i++){
+			int y = i;
+			int x = i;
+
+			// set STI column (in STI_col) based on column of the frame
+			if(i < frameHeight){
+				Vec3b pixel = frame.at<Vec3b>(Point(x_center,y));
+				STI_col.at<Vec3b>(Point(frameNum,i)) = pixel;
+				imshow("STI (Column)", STI_col);
+			}
+
+			// set STI column (in STI_row) based on row of the frame
+			if(i < frameWidth){
+				Vec3b pixel = frame.at<Vec3b>(Point(x,y_center));
+				STI_row.at<Vec3b>(Point(frameNum,i)) = pixel;
+				imshow("STI (Row)", STI_row);
+			}
+		}
+
+		frameNum++;
+	}
+
+	std::cout << "Press Enter to Exit..." << std::endl; // STI generation complete
+	std::cin.get();
+
 	return 0;
 }
